9ee85560527f925b5da9bcdcec4f62c42433241a
alexey <alexey@digger.dev>
2022-05-11 17:07:18 +0100

    update cloudfront configuration

--- service-webapp.template.tf
+++ service-webapp.template.tf
@@ -205,7 +205,7 @@ resource "aws_s3_bucket_policy" "{{service_name}}_website_root_bucket_policy" {
         "s3:GetObject"
       ],
       "Resource": [
-        "${aws_s3_bucket.{{service_name}}_website_root.arn}/*",
+        "${aws_s3_bucket.{{service_name}}_website_root.arn}/*"
       ]
     }
   ]
